9850ac99235de462d999e9539e8c49aa6de652b4
a.lagodny@gmail.com <lagodny>
2026-01-22 21:04:19 +0200

    added touch

--- Sources/VCL/DC.Chart.pas
+++ Sources/VCL/DC.Chart.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   System.Classes, System.Types, System.SysUtils,
-  Winapi.Windows,
+  Winapi.Windows, Winapi.Messages,
   VCL.Controls,
   VCLTee.Chart, VCLTee.TeeProcs, VCLTee.TeCanvas, VCLTee.TeEngine,
   uOPCInterval;
@@ -28,6 +28,17 @@ type
     procedure SetZoomFactor(const Value: Double);
   private
     procedure ProcessMouseWheel(aCoef: Double);
+  private
+    // touch
+    FLastDist: Integer;
+    FLastMidX: Integer;
+    FLastMidY: Integer;
+    FLastDX, FLastDY: Integer;
+    FLastRec: TRect;
+
+    FOldWndProc: TWndMethod;
+    procedure NewWndProc(var Msg: TMessage);
+    procedure WM_Touch(var Msg: TMessage);
   protected
     function DoMouseWheelDown(Shift: TShiftState; MousePos: TPoint): Boolean; override;
     function DoMouseWheelUp(Shift: TShiftState; MousePos: TPoint): Boolean; override;
@@ -36,7 +47,7 @@ type
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
 
-    procedure ZoomByPoint(aPoint: TPoint; aZoomFactor: Extended; aZoomSet: TZoomSet);
+    procedure ZoomByPoint(aPoint: TPoint; aZoomFactorX, aZoomFactorY: Extended; aZoomSet: TZoomSet);
   published
     property RealTime : Boolean read GetRealTime write SetRealTime default False;
     property ZoomFactor: Double read FZoomFactor write SetZoomFactor;
@@ -53,6 +64,7 @@ type
 implementation
 
 uses
+  System.Math,
   DC.SeriesAdapter, DC.SeriesAdapterIntf;
 
 const
@@ -64,6 +76,16 @@ const
 constructor TDCChart.Create(AOwner: TComponent);
 begin
   inherited;
+
+  // touch
+  FLastDist := 0;
+  FLastMidX := 0;
+  FLastMidY := 0;
+
+  FOldWndProc := WindowProc;
+  WindowProc := NewWndProc;
+  RegisterTouchWindow(Handle, 0);
+
 
   FInterval := TOPCInterval.Create;
   FInterval.OnChanged := DoChangeInterval;
@@ -91,6 +113,7 @@ end;
 
 destructor TDCChart.Destroy;
 begin
+  WindowProc := FOldWndProc;
   FInterval.Free;
   inherited;
 end;
@@ -123,6 +146,16 @@ begin
   Result := FRealTime
 end;
 
+procedure TDCChart.NewWndProc(var Msg: TMessage);
+begin
+  case Msg.Msg of
+    Winapi.Messages.WM_TOUCH:
+      WM_Touch(Msg);
+//    Winapi.Messages.WM_MOUSEWHEEL: WM_MouseWheel(Msg);
+  end;
+  FOldWndProc(Msg);
+end;
+
 procedure TDCChart.ProcessMouseWheel(aCoef: Double);
 begin
   if Panning.MouseWheel = pmwNone then
@@ -143,7 +176,7 @@ begin
     else
       aZoomSet := [zsTime];
 
-    ZoomByPoint(p, aCoef, aZoomSet);
+    ZoomByPoint(p, aCoef, aCoef, aZoomSet);
   end;
 end;
 
@@ -181,7 +214,119 @@ begin
   FZoomFactor := Value;
 end;
 
-procedure TDCChart.ZoomByPoint(aPoint: TPoint; aZoomFactor: Extended; aZoomSet: TZoomSet);
+procedure TDCChart.WM_Touch(var Msg: TMessage);
+var
+  Inputs: array of TTouchInput;
+  Count: Integer;
+  X1, Y1, X2, Y2: Integer;
+  dx, dy: Integer;
+  Dist: Integer;
+  MidX, MidY: Integer;
+  MidP: TPoint;
+  aRect: TRect;
+  aZoomFactorX, aZoomFactorY: Double;
+begin
+//  if not FEnabled then Exit;
+
+  Count := Msg.WParam and $FFFF;
+  if Count <> 2 then
+  begin
+    FLastDist := 0;
+    FLastRec := TRect.Empty;
+    Exit;
+  end;
+
+  SetLength(Inputs, Count);
+
+  if GetTouchInputInfo(Msg.LParam, Count, @Inputs[0], SizeOf(TTouchInput)) then
+  begin
+    X1 := Inputs[0].x div 100;
+    Y1 := Inputs[0].y div 100;
+
+    X2 := Inputs[1].x div 100;
+    Y2 := Inputs[1].y div 100;
+
+    dx := X2 - X1;
+    dy := Y2 - Y1;
+    Dist := Round(Sqrt(dx*dx + dy*dy));
+
+    // центр між пальцями
+    MidX := (X1 + X2) div 2;
+    MidY := (Y1 + Y2) div 2;
+
+    if (FLastDist = 0) then
+    begin
+      FLastDist := Dist;
+      FLastMidX := MidX;
+      FLastMidY := MidY;
+      FLastDX := X2 - X1;
+      FLastDY := Y2 - Y1;
+      Exit;
+    end;
+
+    if ABS(FLastDist - Dist) < 5 then
+      Exit;
+
+    if dx = 0 then
+      aZoomFactorX := 1
+    else
+      aZoomFactorX := FLastDX / dx;
+
+    if dy = 0 then
+      aZoomFactorY := 1
+    else
+      aZoomFactorY := FLastDY / dy;
+
+    if aZoomFactorX < 0.8 then
+      aZoomFactorX := 0.8;
+    if aZoomFactorX > 1.2 then
+      aZoomFactorX := 1.2;
+
+    if aZoomFactorY < 0.8 then
+      aZoomFactorY := 0.8;
+    if aZoomFactorY > 1.2 then
+      aZoomFactorY := 1.2;
+
+
+
+    ZoomByPoint(ScreenToClient(Point(MidX, MidY)), aZoomFactorX, aZoomFactorY, [zsTime, zsValue]);
+
+    FLastDist := Dist;
+    FLastMidX := MidX;
+    FLastMidY := MidY;
+    FLastDX := dx;
+    FLastDY := dy;
+
+
+//    aRect := ScreenToClient(Rect(X1, Y2, X2, Y2));
+//    if not (FLastRec.IsEmpty) then
+//    begin
+//      DoZoom(FLastRec.Top, aRect.Top, FLastRec.Bottom, aRect.Bottom, FLastRec.Left, aRect.Left, FLastRec.Right, aRect.Right);
+//    end;
+//    FLastRec := aRect;
+
+//    // масштабування
+//    if (Dist <> 0) and (FLastDist <> 0) then
+//    begin
+//      aZoomFactor := FLastDist/Dist;
+//      if aZoomFactor < 0.1 then
+//        aZoomFactor := 0.1;
+//      if aZoomFactor > 10 then
+//        aZoomFactor := 10;
+//
+//      ZoomByPoint(ScreenToClient(Point(MidX, MidY)), aZoomFactor, [zsTime, zsValue]);
+////    DoPinchZoom(Dist);
+//    end;
+//    FLastDist := Dist;
+
+    // панорамування
+//    DoPan(MidX, MidY);
+
+    CloseTouchInputHandle(Msg.LParam);
+  end;
+end;
+
+procedure TDCChart.ZoomByPoint(aPoint: TPoint; aZoomFactorX, aZoomFactorY: Extended; aZoomSet: TZoomSet);
 var
   i: Integer;
   w1, h1: integer;
@@ -241,8 +386,8 @@ begin
 
   if aZoomSet = [zsTime] then
   begin
-    w1 := Trunc((aZoomRect.Right - aZoomRect.Left) * aZoomFactor) div 2;
-    dw := Trunc((aPoint.X - (aZoomRect.Right + aZoomRect.Left) / 2) * aZoomFactor);
+    w1 := Trunc((aZoomRect.Right - aZoomRect.Left) * aZoomFactorX) div 2;
+    dw := Trunc((aPoint.X - (aZoomRect.Right + aZoomRect.Left) / 2) * aZoomFactorX);
 
     aZoomRect.Left := aPoint.X - w1 - dw;
     aZoomRect.Right := aPoint.X + w1 - dw;
@@ -250,8 +395,8 @@ begin
 
   else if aZoomSet = [zsValue] then
   begin
-    h1 := Trunc((aZoomRect.Bottom - aZoomRect.Top) * aZoomFactor) div 2;
-    dh := Trunc((aPoint.Y - (aZoomRect.Bottom + aZoomRect.Top)/2) * aZoomFactor);
+    h1 := Trunc((aZoomRect.Bottom - aZoomRect.Top) * aZoomFactorY) div 2;
+    dh := Trunc((aPoint.Y - (aZoomRect.Bottom + aZoomRect.Top)/2) * aZoomFactorY);
 
     aZoomRect.Top := aPoint.Y - h1 - dh;
     aZoomRect.Bottom := aPoint.Y + h1 - dh;
@@ -259,14 +404,14 @@ begin
 
   else
   begin
-    w1 := Trunc((aZoomRect.Right - aZoomRect.Left) * aZoomFactor) div 2;
-    dw := Trunc((aPoint.X - (aZoomRect.Right + aZoomRect.Left) / 2) * aZoomFactor);
+    w1 := Trunc((aZoomRect.Right - aZoomRect.Left) * aZoomFactorX) div 2;
+    dw := Trunc((aPoint.X - (aZoomRect.Right + aZoomRect.Left) / 2) * aZoomFactorX);
 
     aZoomRect.Left := aPoint.X - w1 - dw;
     aZoomRect.Right := aPoint.X + w1 - dw;
 
-    h1 := Trunc((aZoomRect.Bottom - aZoomRect.Top) * aZoomFactor) div 2;
-    dh := Trunc((aPoint.Y - (aZoomRect.Bottom + aZoomRect.Top)/2) * aZoomFactor);
+    h1 := Trunc((aZoomRect.Bottom - aZoomRect.Top) * aZoomFactorY) div 2;
+    dh := Trunc((aPoint.Y - (aZoomRect.Bottom + aZoomRect.Top)/2) * aZoomFactorY);
 
     aZoomRect.Top := aPoint.Y - h1 - dh;
     aZoomRect.Bottom := aPoint.Y + h1 - dh;
